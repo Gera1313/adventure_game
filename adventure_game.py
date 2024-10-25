f625a779c8ce34a0a7375f85254a436aab66382a
Mark Anthony <touya1313@hotmail.com>
2024-10-25 06:59:13 -0600

    added intro

---
+++ adventure_game.py
@@ -0,0 +1,10 @@
+# start the game
+
+def intro():
+    print("Welcome to the Mystic Forest!")
+    print("Your mission is to find the hidden treasure and escape safely.")
+    print("Make your choices wisely as danger lurks at every turn!")
+    print("Let's begin the adventure...\n")
+
+# calls the intro function to display messages
+intro()
